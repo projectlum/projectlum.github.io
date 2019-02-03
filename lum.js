9c5f762852a7f1b0fa8e62305d9427f311b3b448
nprezant <34352369+nprezant@users.noreply.github.com>
2019-02-02 21:57:06 -0500

    update search and style etc

--- lum.js
+++ lum.js
@@ -8,4 +8,37 @@ var Startup = /** @class */ (function () {
     return Startup;
 }());
 Startup.main();
+function printme(val) {
+    console.log(val);
+}
+function clearList(root) {
+    while (root.firstChild) {
+        root.removeChild(root.firstChild);
+    }
+}
+function search() {
+    var input = document.getElementById("search-input");
+    var filter = input.value.toUpperCase();
+    var testlist = ['Raleigh', 'Chapel Hill', 'Durham', 'Greensboro', 'Chatanooga', 'Detroit', 'Baltimore', 'Hamilton', 'New Havan', 'North Carolina', 'Vermont', 'Alaska'];
+    var displayList = document.getElementById("search-results-list");
+    // clear the list
+    clearList(displayList);
+    if (filter.length == 0) {
+        displayList.style.visibility = 'hidden';
+        return;
+    }
+    else {
+        displayList.style.visibility = 'visible';
+    }
+    for (var i = 0; i < testlist.length; i++) {
+        var item = testlist[i];
+        if (item.toUpperCase().indexOf(filter) > -1) {
+            var liNode = document.createElement('li');
+            var textNode = document.createTextNode(item);
+            liNode.appendChild(textNode);
+            displayList.appendChild(liNode);
+            console.log(item + 'is a valid search return thing');
+        }
+    }
+}
 //# sourceMappingURL=lum.js.map
--- lum.ts
+++ lum.ts
@@ -6,3 +6,43 @@ class Startup {
 }
 
 Startup.main();
+
+function printme(val: string): void {
+    console.log(val)
+}
+
+
+function clearList(root: any): void {
+    while( root.firstChild ){
+        root.removeChild( root.firstChild );
+    }
+}
+
+
+function search(): void {
+    let input: any = document.getElementById("search-input");
+    let filter: string = input.value.toUpperCase();
+
+    let testlist = ['Raleigh', 'Chapel Hill', 'Durham', 'Greensboro', 'Chatanooga', 'Detroit', 'Baltimore', 'Hamilton', 'New Havan', 'North Carolina', 'Vermont', 'Alaska']
+    let displayList = document.getElementById("search-results-list")
+
+    // clear the list
+    clearList(displayList);
+    if (filter.length == 0) {
+        displayList.style.visibility = 'hidden';
+        return;
+    } else {
+        displayList.style.visibility = 'visible';
+    }
+
+    for (let i = 0; i < testlist.length; i++) {
+        let item = testlist[i];
+        if (item.toUpperCase().indexOf(filter) > -1) {
+            let liNode = document.createElement('li');
+            let textNode = document.createTextNode(item);
+            liNode.appendChild(textNode);
+            displayList.appendChild(liNode);
+            console.log(item + 'is a valid search return thing');
+        }
+    }
+}
